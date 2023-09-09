138eb01e79f9c56b369298b3e9247ea98f272617
PVBiz <138363951+PVBiz@users.noreply.github.com>
2023-09-09 14:39:18 +0200

    Updated Hidden Dynamodb

--- js/availability-config.js
+++ js/availability-config.js
@@ -1,8 +1,8 @@
 // config.js (Not publicly accessible)
 
 const dynamoDBConfig = {
-    accessKeyId: 'AKIAZZOLPERQCTDDWOHQ',
-    secretAccessKey: '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',
+    accessKeyId: 'xxxxxx',
+    secretAccessKey: 'xxxxxxxxxxxx+DBvf0I2KZl',
     region: 'eu-central-1',
     tableName: 'PVBIZ_SOLAR_ENV_STATUS',
 };
