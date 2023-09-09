61cfdcee1b6a0daf8915eebf126f4c3362980ec3
PVBiz <138363951+PVBiz@users.noreply.github.com>
2023-09-09 14:07:51 +0200

    Updated

--- js/availability.js
+++ js/availability.js
@@ -1,5 +1,6 @@
 import dynamoDBConfig from './availability-config.js'; // Import your DynamoDB configuration
 //import AWS from 'aws-sdk';
+//TEST
 
 AWS.config.update(dynamoDBConfig);
 const dynamoDB = new AWS.DynamoDB();
