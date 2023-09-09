1f3b01c1542fb6adb023fe07e2cf192cead2e672
PVBiz <138363951+PVBiz@users.noreply.github.com>
2023-09-09 14:09:58 +0200

    Added table name

--- js/availability-config.js
+++ js/availability-config.js
@@ -4,6 +4,7 @@ const dynamoDBConfig = {
     accessKeyId: 'PVBIZ_DYNAMODB',
     secretAccessKey: 'YAKIAZZOLPERQPPIGGJHD',
     region: 'eu-central-1'
+    tableName: 'PVBIZ_SOLAR_ENV_STATUS',
 };
 
 export default dynamoDBConfig;
