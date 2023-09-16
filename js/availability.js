bcdf3ba3e9df4586776b9036c05b71da185098c9
PVBiz <138363951+PVBiz@users.noreply.github.com>
2023-09-16 13:14:09 +0200

    Updated to google sheet

--- js/availability.js
+++ js/availability.js
@@ -1,52 +1,47 @@
-function updateInverterData() {
-    const fifteenMinutes = 15 * 60 * 1000; // 15 minutes in milliseconds
-    const oneHour = 60 * 60 * 1000; // 1 hour in milliseconds
+// Install the Sheetrock.js library
+const Sheetrock = require('sheetrock');
 
-    // Define your inverter names and corresponding HTML element IDs
-    const inverters = [
-        { name: 'SUNSYNK', field: 'field1', updateTimeId: 'updateTime1' },
-        { name: 'DEYE', field: 'field4', updateTimeId: 'updateTime2' },
-        { name: 'SOLARMAN', field: 'field7', updateTimeId: 'updateTime3' }
-    ];
+// Create a new Sheetrock object
+const sheetrock = new Sheetrock({
+  spreadsheetId: '1UsgZQY_S36ZuOTCW8hG-p05q2wlzcykAFRF-Ntjf0uU',
+});
 
-    // Fetch and parse the data from availability_data.txt
-    ////
-    fetch('js/data/availability.txt')
-        .then(response => response.text())
-        .then(data => {
-            try {
-                const jsonData = JSON.parse(data);
-                for (const inverter of inverters) {
-                    if (jsonData.hasOwnProperty(inverter.name)) {
-                        const updateTime = jsonData[inverter.name];
-                        const dataField = document.getElementById(inverter.field);
-                        const updateTimeElement = document.getElementById(inverter.updateTimeId);
+// Define your inverter names and corresponding HTML element IDs
+const inverters = [
+  { name: 'SUNSYNK', field: 'field1', updateTimeId: 'updateTime1' },
+  { name: 'DEYE', field: 'field4', updateTimeId: 'updateTime2' },
+  { name: 'SOLARMAN', field: 'field7', updateTimeId: 'updateTime3' }
+];
 
-                        // Update the HTML elements with data
-                        dataField.textContent = updateTime;
-                        updateTimeElement.textContent = updateTime;
+// Update the inverter data
+async function updateInverterData() {
+  try {
+    // Read the data from the Google Sheet
+    const values = await sheetrock.get('Sheet1');
 
-                        // Calculate and apply the CSS class based on time difference
-                        const timestamp = new Date(updateTime);
-                        const currentTime = new Date();
-                        const timeDifference = currentTime - timestamp;
+    // Update the HTML elements with data
+    for (const inverter of inverters) {
+      const updateTime = values[0][inverter.field];
+      const dataField = document.getElementById(inverter.field);
+      const updateTimeElement = document.getElementById(inverter.updateTimeId);
 
-                        if (timeDifference > oneHour) {
-                            dataField.classList.add('old');
-                        } else if (timeDifference > fifteenMinutes) {
-                            dataField.classList.remove('old');
-                        }
-                    } else {
-                        console.error(`No data found for ${inverter.name}`);
-                    }
-                }
-            } catch (error) {
-                console.error('Error parsing JSON data:', error);
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+      dataField.textContent = updateTime;
+      updateTimeElement.textContent = updateTime;
+
+      // Calculate and apply the CSS class based on time difference
+      const timestamp = new Date(updateTime);
+      const currentTime = new Date();
+      const timeDifference = currentTime - timestamp;
+
+      if (timeDifference > oneHour) {
+        dataField.classList.add('old');
+      } else if (timeDifference > fifteenMinutes) {
+        dataField.classList.remove('old');
+      }
+    }
+  } catch (error) {
+    console.error('Error fetching data from Google Sheets:', error);
+  }
 }
 
 // Update data initially and then every 5 minutes (adjust as needed)
